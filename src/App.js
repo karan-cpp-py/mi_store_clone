abd18e1778a7bcd3a232e0a8fde89790421b13cf
karan-cpp-py <karancpppython@gmail.com>
2023-01-28 04:19:33 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import React from 'react'
+import PreNavbar from './components/PreNavbar';
+import Navbar from './components/Navbar';
+import Slider from './components/Slider.js'
+import { BrowserRouter as Router } from 'react-router-dom';
+import data from "./data/data.json"
+import Offers from "./components/Offers.js"
+import Heading from './components/Heading';
+import StarProduct from './components/StarProduct';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <PreNavbar />
+      <Navbar />
+      <Slider start={data.banner.start}/>
+      <Offers offer={data.offer}/>
+      <Heading text="STAR PRODUCTS" />
+      <StarProduct starProduct={data.starProduct} />
+    </Router>
   );
 }
 
